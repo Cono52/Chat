3fed833ed5954152206606e801ef90fe2431eb2b
Conor O'Flanagan <oflanac52@gmail.com>
2018-11-27 23:59:36 +0000

    add react testing library

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import { render, cleanup } from "react-testing-library";
+import "jest-dom/extend-expect";
+
+// automatically unmount and cleanup DOM after the test is finished.
+afterEach(cleanup);
+
+test("Renders the app on the username input screen", () => {
+  const { getByText } = render(<App />);
+  expect(getByText("What will your username be?")).toBeInTheDOM();
+  const inputBox = document.querySelector('[name="userName"]');
+  expect(inputBox).toBeInTheDocument();
 });
