731988f2c2d44739887d682dcdc4fb65e452b1c9
Conor O'Flanagan <oflanac52@gmail.com>
2018-12-01 20:39:46 +0000

    write a bit more of a test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,8 @@
 import React from "react";
 import App from "./App";
+import mockio, { serverSocket, cleanUp } from "../mocks/socket.io-client";
 
-import { render, cleanup } from "react-testing-library";
+import { render, cleanup, fireEvent, debug } from "react-testing-library";
 import "jest-dom/extend-expect";
 
 // automatically unmount and cleanup DOM after the test is finished.
@@ -12,4 +13,14 @@ test("Renders the app on the username input screen", () => {
   expect(getByText("What will your username be?")).toBeInTheDOM();
   const inputBox = document.querySelector('[name="userName"]');
   expect(inputBox).toBeInTheDocument();
+});
+
+test("Sending user name will attempt to join a chatroom", () => {
+  const utils = render(<App />);
+  const inputBox = document.querySelector('[name="userName"]');
+  const submitButton = document.querySelector("button");
+  inputBox.value = "testName";
+  fireEvent.change(inputBox);
+  fireEvent.click(submitButton);
+  // TODO: check if socket event was fired
 });
